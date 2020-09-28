1cac8c248807d0c70b614667f22c9f0b677241ec
Murilo Arruda <muriloarrudap@gmail.com>
2020-09-28 15:54:31 -0300

    Convert to typescript

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,7 +6,13 @@ import Link from 'next/link';
 const name = 'Murilo Arruda';
 export const siteTitle = 'Murilo Arruda Learning Next.js';
 
-export default function Layout({ children, home }) {
+export default function Layout({
+  children,
+  home,
+}: {
+  children: React.ReactNode;
+  home?: boolean;
+}) {
   return (
     <div className={styles.container}>
       <Head>
